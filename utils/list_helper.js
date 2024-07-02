5c3065d1c8775b4c82c0acf45f30a24a0aebf06a
Miki Kauste <miki.kauste@gmail.com>
2024-07-02 23:15:58 +0300

    teht 4.3-4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,46 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else if (blogs.length > 1) {
+    return blogs.reduce((sum, blog) => blog.likes + sum, 0)
+  }
+}
+
+const favoriteBlog = (blogs) => {
+  const mostLikes = blogs.reduce((likes, blog) => Math.max(blog.likes, likes),0)
+  const filteredBlogs = blogs.filter(blog => blog.likes === mostLikes)
+  return filteredBlogs[0]
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
+
+/*const reverse = (string) => {
+    return string
+      .split('')
+      .reverse()
+      .join('')
+  }
+  
+  const average = (array) => {
+    const reducer = (sum, item) => {
+      return sum + item
+    }
+  
+    return array.reduce(reducer, 0) / array.length
+  }
+  
+  module.exports = {
+    reverse,
+    average,
+  }
+*/
