c4d61f9019c73113181ecff272488c9aeb33022e
Miki Kauste <miki.kauste@gmail.com>
2024-07-24 01:18:08 +0300

    Osan d tehtäviä

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,29 @@
 const blogsRouter = require('express').Router()
-const Blog = require('../models/blog') 
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {username: 1, name:1})
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   if ((body.title === undefined) || (body.url === undefined)) {
     response.status(400).end()
   } else {
@@ -16,9 +31,12 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id,
   })
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
   }
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -23,24 +23,3 @@ module.exports = {
   totalLikes,
   favoriteBlog
 }
-
-/*const reverse = (string) => {
-    return string
-      .split('')
-      .reverse()
-      .join('')
-  }
-  
-  const average = (array) => {
-    const reducer = (sum, item) => {
-      return sum + item
-    }
-  
-    return array.reduce(reducer, 0) / array.length
-  }
-  
-  module.exports = {
-    reverse,
-    average,
-  }
-*/
